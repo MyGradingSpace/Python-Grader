ebcc48bbef275890708da699c929f795296d1964
Fangjian Lei <leix5490@gmail.com>
2020-07-21 14:32:04 -0400

    add unzip function

--- download.py
+++ download.py
@@ -1,8 +1,13 @@
 import urllib.request
 import os
+from zipfile import ZipFile
+
 
 cwd = os.getcwd()
-cwd = cwd + "\demo.txt"
+cwd = cwd + "\demo.zip"
 print(cwd)
-url="https://github.com/fleiray/Fangjian/files/4951342/command.txt"
+url="https://github.com/fleiray/Fangjian/files/4954930/command.zip"
 urllib.request.urlretrieve(url,cwd)
+
+with ZipFile("demo.zip","r") as zipobj:
+    zipobj.extractall("extracted_forlder")
