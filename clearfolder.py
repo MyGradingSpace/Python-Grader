3f5c4ffe82f738f2fa1dafc901fa37e0dae0bb8b
Fangjian Lei <leix5490@gmail.com>
2020-07-23 01:52:54 -0400

    adding clear folder function and starting error handling

--- clearfolder.py
+++ clearfolder.py
@@ -1,3 +1,11 @@
+import shutil
 import os
 
-shutil.rmtree("")
+cwd = os.getcwd()
+cwd = os.path.join(cwd , "testing_folder")
+
+print(cwd)
+try :
+    shutil.rmtree(cwd)
+except OSError as e:
+    print ("Error: %s - %s." % (e.filename, e.strerror))
